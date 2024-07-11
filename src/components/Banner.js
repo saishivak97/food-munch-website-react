4113aed5cabef6cb7557a51ce633e463d5501f34
KONETI SAI SHIVA <144230586+saishivak97@users.noreply.github.com>
2024-07-12 00:08:22 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = () => (
-  <div class="banner-section-bg-container">
-  <div class="text-center">
-    <h1 class="banner-heading">Get Delicious Food Anytime</h1>
-    <p class="banner-caption">Eat Smart & Healthy</p>
-    <button class="custom-button">View Menu</button>
-    <button class="custom-outline-button">Order Now</button>
+  <div className="banner-section-bg-container">
+  <div className="text-center">
+    <h1 className="banner-heading">Get Delicious Food Anytime</h1>
+    <p className="banner-caption">Eat Smart & Healthy</p>
+    <button className="custom-button">View Menu</button>
+    <button className="custom-outline-button">Order Now</button>
   </div>
 </div>
 );
